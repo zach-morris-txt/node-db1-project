ca2ba325359e541ff81a97d9e65e34c085e007f9
unknown <zach-morris@lambdastudents.com>
2021-07-12 20:21:46 -0400

    Middleware, Model

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,38 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
   // DO YOUR MAGIC
+  return db('accounts')
 }
 
-const getById = id => {
+async function getById(id) {
   // DO YOUR MAGIC
+  const result = await db('accounts')
+    .where('id', id).first()
+  return result
 }
 
-const create = account => {
+const create = async account => {
   // DO YOUR MAGIC
+  const [id] = await db('accounts')
+    .insert(account)
+  return getById(id)
+}
+async function updateById(id, {name, budget}) {
+  // DO YOUR MAGIC
+  await db('accounts')
+    .where('id', id)
+    .update({name, budget})
+  return getById(id)
 }
 
-const updateById = (id, account) => {
+async function deleteById(id) {
   // DO YOUR MAGIC
-}
-
-const deleteById = id => {
-  // DO YOUR MAGIC
+  const toDelete = await getById(id)
+  await db('accounts')
+    .where({id})
+    .del()
+  return toDelete
 }
 
 module.exports = {
--- queries.sql
+++ queries.sql
@@ -1,16 +1,23 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
+SELECT * FROM Customers WHERE postalcode = 1010
 
 -- Find the phone number for the supplier with the id 11
+SELECT phone FROM Suppliers WHERE supplierid = 11
 
 -- List first 10 orders placed, sorted descending by the order date
+SELECT * FROM Orders ORDER BY orderdate DESC LIMIT 10
 
 -- Find all customers that live in London, Madrid, or Brazil
+SELECT * FROM Customers WHERE city = 'London' OR city = 'Madrid' OR country = 'Brazil'
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+INSERT INTO Customers (customername, contactname, address, city, postalcode, country)
+VALUES ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth')
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+UPDATE Customers SET postalcode = '11122' WHERE customername = 'The Shire'
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
