4dcf6a038f650e43bb944de61c4f22cf858fca69
unknown <zach-morris@lambdastudents.com>
2021-07-12 20:26:18 -0400

    Router

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,69 @@
+const express = require('express')
+
+const Account = require('./accounts-model')
 const router = require('express').Router()
 
-router.get('/', (req, res, next) => {
+const md = require('./accounts-middleware')
+
+
+router.get('/', async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const data = await Account.getAll()
+    res.json(data)
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', 
+  md.checkAccountId, 
+  async (req, res, next) => {
   // DO YOUR MAGIC
+  res.json(req.account)
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', 
+  md.checkAccountPayload, 
+  md.checkAccountNameUnique, 
+  async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const data = await Account.create(req.body)
+    res.status(201).json(data)
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', 
+  md.checkAccountId, 
+  md.checkAccountPayload, 
+  async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const data = await Account.updateById(req.params.id, req.body)
+    res.json(data)
+  } catch (err) {
+    next(err)
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', 
+  md.checkAccountId, 
+  async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const data = await Account.deleteById(req.params.id)
+    res.json(data)
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.use((err, req, res, next) => {
   // DO YOUR MAGIC
+  res.status(err.status || 500).json({ message: err.message,})
 })
 
 module.exports = router;
